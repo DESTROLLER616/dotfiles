d2adaa3493647dd2d0cd4999308bb7fee857834b
DESTROLLER616 <cristopher.cgca22@gmail.com>
2026-05-17 14:19:56 -0600

    feat: Add fastfetch config and other changes from nvim

--- nvim/lua/plugins/imports/utils.lua
+++ nvim/lua/plugins/imports/utils.lua
@@ -1,5 +1,4 @@
 return {
-	{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
 	{ "nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = { "nvim-lua/plenary.nvim" } },
 }
